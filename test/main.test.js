245f7aeac237702b9c20ca511958b0242e789360
Jack Steam <jacksteamdev@gmail.com>
2018-01-03 11:08:42 -0600

    Refactor and comment tests

--- test/main.test.js
+++ test/main.test.js
@@ -3,10 +3,10 @@ import _ from 'lodash/fp'
 import createTrees from '../src/main'
 
 const items = [
-  { top: 4, right: 4, bottom: 2, left: 2 },
-  { top: 6, right: 6, bottom: 5, left: 5 },
-  { top: 2, right: 2, bottom: 1, left: 1 },
-  { top: 10, right: 10, bottom: 0, left: 0 }
+  { bottom: 2, top: 4, left: 2, right: 4 },
+  { bottom: 5, top: 6, left: 5, right: 6 },
+  { bottom: 1, top: 2, left: 1, right: 2 },
+  { bottom: 0, top: 10, left: 0, right: 1 }
 ]
 
 const keys = ['bottom', 'top', 'left', 'right']
@@ -14,7 +14,7 @@ const keys = ['bottom', 'top', 'left', 'right']
 it('should create trees', () => {
   const result = createTrees(keys, items)
   expect(result).toBeInstanceOf(Function)
-  expect(result.count).toBe(2)
+  expect(result.count).toBe(2) // Two trees
   expect(result.trees.length).toBe(result.count)
   expect(result.trees[0].count).toBe(4)
   expect(result.trees[1].count).toBe(4)
